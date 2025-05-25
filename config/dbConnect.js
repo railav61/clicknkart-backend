ef7a46e36de9fc0b8eb9e4bbeea717988bb44b0a
railav61 <mritunjayrai102004@gmail.com>
2025-05-26 00:45:35 +0530

    mongodb path update

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,8 +1,9 @@
 const { default: mongoose } = require("mongoose");
-
+require("dotenv").config();
 const dbConnect = () => {
+  const MONGOURI = process.env.MONGOURI;
   try {
-    const conn = mongoose.connect("mongodb://localhost:27017/");
+    const conn = mongoose.connect(MONGOURI || "mongodb://localhost:27017/");
     console.log("Database Connected Successfully");
   } catch (error) {
     console.log("DAtabase error");
